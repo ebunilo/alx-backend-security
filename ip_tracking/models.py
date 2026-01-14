91b7cb75fb7f1b9cef66851f4a2a99682acb30c1
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2026-01-14 11:42:31 +0100

    Middleware to log IP address

---
+++ ip_tracking/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class RequestLog(models.Model):
+    ip_address = models.CharField(max_length=45)  # IPv6 can be up to 45 chars
+    timestamp = models.DateTimeField(auto_now_add=True)
+    path = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.path} at {self.timestamp}"
